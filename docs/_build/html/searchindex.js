5eb392548b3970a748928fedbff914f03283aec5
mehmadi <mehdi@Mehdi.local>
2016-04-17 10:58:52 +0430

    fully changed the structure of the documentation
    fully changed the structure of the documentation

--- docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to SWAT-LAMP&#8217;s documentation!"],titleterms:{document:0,indic:0,lamp:0,swat:0,swat_lamp:[],tabl:0,welcom:0}})
